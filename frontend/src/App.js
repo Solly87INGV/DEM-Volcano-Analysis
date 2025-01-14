c7e71d57ee960b803e89fa357de9794919cf2926
Solly87INGV <marco.solinas@ingv.it>
2025-01-14 08:30:03 +0100

    fix/layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Description from './components/Description';
@@ -17,6 +17,25 @@ function App() {
     setProcessId(null);
     setDemFile(null);
   };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      // Verifica se Ctrl + 0 è stato premuto
+      if (event.ctrlKey && event.key === '0') {
+        event.preventDefault(); // Previene il comportamento predefinito
+        // Puoi aggiungere qui ulteriori logiche se necessario
+        console.log('Ctrl + 0 premuto: comportamento prevenuto per stabilità dell\'app.');
+      }
+    };
+
+    // Aggiungi il listener quando il componente è montato
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Rimuovi il listener quando il componente è smontato
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   return (
     <div className="App">
